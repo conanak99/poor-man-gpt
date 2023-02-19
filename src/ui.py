e11e5ff8f3d3aae1fb4a9437043731b0b09cc3a4
conanak99 <huyhoang8a5@gmail.com>
2023-02-19 17:10:07 +0800

    make bot keep linebreak

--- src/ui.py
+++ src/ui.py
@@ -4,24 +4,36 @@ from src.base import Message
 import src.constants  # Import ENV variables from .env
 from src.completion import generate_completion_response
 
+TEST_MODE = False  # This should be False all the time. Only set it to true for UI testing
+
 logging.basicConfig(
     format="[%(asctime)s] [%(filename)s:%(lineno)d] %(message)s", level=logging.INFO
 )
 
 
 def callGPT(original_message, history):
-    yield None, history + [(original_message, '')], history
-
     messages = []
     for message in original_message.split('\n'):
         messages.append(Message(user="Harry", text=message))
+
+    # Keep linebreak
+    original_message = original_message.replace('\n', '<br>')
+
+    if TEST_MODE:
+        # Do whatever test here
+        test_message = 'hello <br> world <br> from <br> bot <br>' + original_message
+        history.append((original_message, test_message))
+        yield None, history, history
+        return
+
+    yield None, history + [(original_message, '')], history
+
     response_data = generate_completion_response(messages=messages)
 
-    response = ""
-    result = ""
+    response, result = "", ""
     for data in response_data:
         response = response + data.text
-        result = '.\n'.join(response.split('. ')).strip()
+        result = '.<br>'.join(response.split('. ')).strip()
         yield None, history + [(original_message, result)], history
 
     history.append((original_message, result))
